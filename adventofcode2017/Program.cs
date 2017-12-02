2fdebae3fab17a93908d8c0a537adfa9aa4aed9b
Belterius <gandbri@gmail.com>
2017-12-02 15:41:27 +0100

    Day1 & 2 of adventofcode

--- adventofcode2017/Program.cs
+++ adventofcode2017/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,136 @@ namespace adventofcode2017
     {
         static void Main(string[] args)
         {
+            //Day1_Output();
+            Day2_Output();
         }
 
-        public int Day1_InverseCaptcha(string captcha)
+
+        public static void Day1_Output()
         {
-
+            string myString = "494751136895345894732582362629576539599184296195318162664695189393364372585778868512194863927652788149779748657989318645936221887731542718562643272683862627537378624843614831337441659741281289638765171452576466381314558821636595394981788588673443769343597851883955668818165723174939893841654914556681324133667446412138511724424292394454166623639872425168644336248217213826339741267546823779383343362789527461579565822966859349777937921933694912369552152772735167832762563719664315456987186713541153781499646178238762644186484381142249926194743713139262596264878458636595896487362658672224346241358667234115974528626523648311919886566497837217169673923935143386823757293148719377821517314629812886912412829924484513493885672343964151252433622341141661523814465991516961684511941471572895453711624986269342398786175846925783918686856442684489873327497698963658862856336682422797551251489126661954848572297228765445646745256499679451426358865477844467458533962981852292513358871483321161973583245698763531598395467675529181496911117769834127516441369261275244225978893617456524385518493112272169767775861256649728253754964675812534546226295535939697352141217337346738553495616832783757866928174519145357234834584788253893618549484385733283627199445369658339175644484859385884574943219267922729967571943843794565736975716174727852348441254492886794362934343868643337828637454277582276962353246357835493338372219824371517526474283541714897994127864461433627894831268659336264234436872715374727211764167739169341999573855627775114848275268739159272518673316753672995297888734844388928439859359992475637439771269232916542385876779616695129412366735112593669719335783511355773814685491876721452994714318863716542473187246351548626157775143333161422867924437526253865859969947366972895674966845993244925218766937543487875485647329995285821739359369998935331986126873726737672159265827566443794515755939813676194755474477224152139987944419463371386499841415227734673733555261543871359797796529847861748979527579985757964742667473767269248335229836818297477665453189662485548925521497365877771665365728224394427883312135322325169141784";
+            Console.WriteLine($"Part One :");
+            Console.WriteLine($"Result for 1122 : {Day1_InverseCaptcha_PartTwo("1122").ToString()}");
+            Console.WriteLine($"Result for 1111 : {Day1_InverseCaptcha_PartTwo("1111").ToString()}");
+            Console.WriteLine($"Result for 1234 : {Day1_InverseCaptcha_PartTwo("1234").ToString()}");
+            Console.WriteLine($"Result for 91212129 : {Day1_InverseCaptcha_PartTwo("91212129").ToString()}");
+            Console.WriteLine($"Result for my string : {Day1_InverseCaptcha_PartTwo(myString).ToString()}");
+            Console.WriteLine($"Part Two :");
+            Console.WriteLine($"Result for 1212 : {Day1_InverseCaptcha_PartTwo("1212").ToString()}");
+            Console.WriteLine($"Result for 1221 : {Day1_InverseCaptcha_PartTwo("1221").ToString()}");
+            Console.WriteLine($"Result for 123425 : {Day1_InverseCaptcha_PartTwo("123425").ToString()}");
+            Console.WriteLine($"Result for 123123 : {Day1_InverseCaptcha_PartTwo("123123").ToString()}");
+            Console.WriteLine($"Result for 12131415 : {Day1_InverseCaptcha_PartTwo("12131415").ToString()}");
+            Console.WriteLine($"Result for my string : {Day1_InverseCaptcha_PartTwo(myString).ToString()}");
+            Console.Read();
         }
+
+        public static int Day1_InverseCaptcha_PartOne(string captcha)
+        {
+            int currentNumber = 0;
+            int previousNumber = int.Parse(captcha[captcha.Length - 1].ToString());
+            int correctSum = 0;
+
+            foreach(char c in captcha)
+            {
+                currentNumber = int.Parse(c.ToString());
+                if (currentNumber == previousNumber)
+                {
+                    correctSum += previousNumber;
+                }
+                previousNumber = currentNumber;
+            }
+            return correctSum;
+        }
+
+        public static int Day1_InverseCaptcha_PartTwo(string captcha)
+        {
+            int currentNumber = 0;
+            int correctSum = 0;
+
+            for(int i=0; i<captcha.Length/2; i++)
+            {
+                currentNumber = int.Parse(captcha[i].ToString());
+                if(currentNumber == int.Parse(captcha[i + captcha.Length / 2].ToString()))
+                {
+                    correctSum += 2 * currentNumber;
+                }
+            }
+            return correctSum;
+        }
+
+        public static void Day2_Output()
+        {
+            Console.WriteLine($"Part One :");
+            Console.WriteLine($"Sum of my spreadsheet : {Day2_CorruptionChecksum_PartOne("C:\\Users\\loic\\Desktop\\adventDay2.txt")}");
+            Console.WriteLine($"Part Two :");
+            Console.WriteLine($"Sum of my spreadsheet : {Day2_CorruptionChecksum_PartTwo("C:\\Users\\loic\\Desktop\\adventDay2.txt")}");
+            Console.Read();
+        }
+
+        public static int Day2_CorruptionChecksum_PartOne(string path)
+        {
+            int checksum = 0;
+
+            string[] file = File.ReadAllLines(path);
+            var lines = file.Select(line => line.Split('\t'));
+            var linesValue = lines.Select(line => line.Select(x => int.Parse(x)));
+            var lineSum = linesValue.Select(line => line.Max() - line.Min());
+
+            return lineSum.Sum();
+        }
+        public static int Day2_CorruptionChecksum_PartTwo(string path)
+        {
+            int checksum = 0;
+
+            string[] file = File.ReadAllLines(path);
+            var lines = file.Select(line => line.Split('\t'));
+            var linesValue = lines.Select(line => line.Select(x => int.Parse(x)));
+            return Day2_EvenlyDivise(linesValue.Select(line => line.ToArray()));
+        }
+
+        private static int Day2_EvenlyDivise(IEnumerable<int[]> enumerable)
+        {
+            int correctResult = 0;
+
+            foreach(int[] i in enumerable)
+            {
+                foreach(int value in i)
+                {
+                    foreach(int valueCompare in i)
+                    {
+                        int resultDiv = divisibleValue(value, valueCompare);
+                        if (resultDiv > 1)
+                        {
+                            correctResult += resultDiv;
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            return correctResult;
+        }
+
+        private static int divisibleValue(int a, int b)
+        {
+            if(a > b)
+            {
+                if(a%b == 0)
+                {
+                    return a / b;
+                }
+            }
+            if(b < a)
+            {
+                if(b%a == 0)
+                {
+                    return b / a;
+                }
+            }
+            return 0;
+        }
+
     }
 }
